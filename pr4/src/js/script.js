5b52b159ec3b4d915d30cc2062e8639417047ba9
ValiHustle <89357180+ValiHustle@users.noreply.github.com>
2023-01-23 12:42:17 +0300

    Add files via upload

---
+++ pr4/src/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 1400,
+        cssEase: 'linear',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 2,
+                focusOnSelect: true,
+                autoplay: true,
+                autoplaySpeed: 1400,
+                cssEase: 'linear',
+              }
+            },
+            {
+                breakpoint: 475,
+                settings: {
+                  centerMode: true,
+                  centerPadding: '0px',
+                  slidesToShow: 1,
+                  focusOnSelect: true,
+                  autoplay: true,
+                  autoplaySpeed: 1400,
+                  cssEase: 'linear',
+                }
+              },
+        ]
+    });    
+});
